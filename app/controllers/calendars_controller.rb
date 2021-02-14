6ce3e6e8066c8f4f7ba90dd27db78093dd835db2
karasawa-toshiya <n89.karasawa.toshiya@gmail.com>
2021-02-14 17:30:39 +0900

    Issue4~6を解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,10 +36,10 @@ class CalendarsController < ApplicationController
 
 
       wday_num = Date.today.wday
-      if wday_num >= 7
+      if wday_num + x >= 7
         wday_num = wday_num - 7
       end
-      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date + x).wday]}
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num + x]}
       @week_days.push(days)
     end
 
